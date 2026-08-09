d45a25c034304862fa9c6e5362fddf1562f0d6b7
abdelrhman632 <abdelrhmany200@gmail.com>
2026-08-09 14:47:51 +0300

    Update products.cs
    Changed column data type to match DB

--- NexFit.Backend/models/products.cs
+++ NexFit.Backend/models/products.cs
@@ -3,29 +3,37 @@ namespace NexFit.Backend.Models.Products;
 public class Product
 {
     public int ProductID { get; set; }
+
     public string ProductName { get; set; } = string.Empty;
     public string ProductBrand { get; set; } = string.Empty;
     public string ProductModel { get; set; } = string.Empty;
     public string ProductSKU { get; set; } = string.Empty;
     public string ProductCategory { get; set; } = string.Empty;
     public string ProductGender { get; set; } = string.Empty;
-    public string ProductPrice { get; set; } = string.Empty;
+
+    public decimal ProductPrice { get; set; }
+
     public string ProductMaterial { get; set; } = string.Empty;
     public string ProductUsage { get; set; } = string.Empty;
     public string ProductSurface { get; set; } = string.Empty;
     public string ProductSupportType { get; set; } = string.Empty;
     public string ProductCushioning { get; set; } = string.Empty;
     public string ProductBreathability { get; set; } = string.Empty;
-    public string ProductWeight { get; set; } = string.Empty;
-    public string ProductWaterproof { get; set; } = string.Empty;
+
+    public decimal ProductWeight { get; set; }
+
+    public bool ProductWaterproof { get; set; }
+
     public string ProductDescription { get; set; } = string.Empty;
+
     public string RecommendedDistance { get; set; } = string.Empty;
     public string ArchType { get; set; } = string.Empty;
     public string FootStrike { get; set; } = string.Empty;
     public string EnergyReturn { get; set; } = string.Empty;
-    public string ReleaseYear { get; set; } = string.Empty;
-    public string HeelDropMM { get; set; } = string.Empty;
+
+    public int ReleaseYear { get; set; }
+
+    public int HeelDropMM { get; set; }
+
     public string Terrain { get; set; } = string.Empty;
 }
-
-   
